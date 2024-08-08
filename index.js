214de15c499453d16e39419d6c1ccb7cdf9f7c78
Aryasuta CM <101690903+Aryasuta@users.noreply.github.com>
2024-08-08 16:35:41 +0700

    hanif

---
+++ index.js
@@ -0,0 +1,16 @@
+const imgDiv = document.querySelector('.user-img');
+const img = document.querySelector('#photo');
+const file = document.querySelector('#file');
+const uploadbtn = document.querySelector('#upload-button');
+
+file.addEventListener('change', function(){
+const chosedfile = this.files[0];
+if(chosedfile){
+    const reader = new FileReader();
+
+    reader.addEventListener('load', function(){
+        img.setAttribute('src', reader.result);
+    })
+reader.readAsDataURL(chosedfile);
+    }
+})

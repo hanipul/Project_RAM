b5baef32345f44b732b1397aedcf5751911f5446
Aryasuta CM <aryasutamadana@gmail.com>
2024-08-14 14:42:14 +0700

    backend

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const path = require('path');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+// Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'project_ram'
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to database');
+});
+
+// Signup form routing
+app.post('/signup', (req, res) => {
+    const { nik, nama, nomor, email, telegram, unit, witel } = req.body;
+
+    const sql = 'INSERT INTO users (nik, nama, nomor, email, telegram, unit, witel) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    db.query(sql, [nik, nama, nomor, email, telegram, unit, witel], (err, result) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Server error, please try again later.');
+            return;
+        }
+        res.redirect('/login.html');
+    });
+});
+
+// Run the server
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
---
+++ frontend/index.js
@@ -0,0 +1,26 @@
+const imgDiv = document.querySelector('.user-img');
+const img = document.querySelector('#photo');
+const file = document.querySelector('#file');
+const uploadbtn = document.querySelector('#upload-button');
+
+file.addEventListener('change', function(){
+const chosedfile = this.files[0];
+if(chosedfile){
+    const reader = new FileReader();
+
+    reader.addEventListener('load', function(){
+        img.setAttribute('src', reader.result);
+    })
+reader.readAsDataURL(chosedfile);
+    }
+})
+
+$(function() {
+    $("#date-picker").multiDatesPicker({
+        dateFormat: "yy-mm-dd"
+    });
+});
+
+document.getElementById('sidebarToggle').addEventListener('click', function () {
+    document.querySelector('.offcanvas-wrapper').classList.toggle('open');
+});
